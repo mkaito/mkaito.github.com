0fbb312c92588f3924120635eacd3494466533ad
Chris Webstar <chris.webstar@gmail.com>
2010-06-15 05:48:52 +0100

    Separated console calls from the header bg function

--- js/site.js
+++ js/site.js
@@ -1,20 +1,21 @@
-// mkaito's blog scripting stuff
+// mkaito blog scripting stuff
 // Uses jquery 1.4.2
 
-var setRandomHeaderBG = function() {
-    $("#container > header").css("background-image", function(index, value) {
-	var backgrounds = ['bg_steampunk_01.jpg',
+var getRandomHeaderBG = function() {
+    	var backgrounds = ['bg_steampunk_01.jpg',
 			   'bg_steampunk_02.jpg',
 			   'bg_steampunk_03.jpg'];
 	
 	var randBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 	var randomHeaderBgPath = "url('/images/site/headers/" + randBg + "')";
-	console.debug("setRandomHeaderBG path: %s", randomHeaderBgPath);
-	return randomHeaderBgPath;});
+	return randomHeaderBgPath;
 };
 
 $(document).ready(function(){
     
     // Set a random background image in the header
-    setRandomHeaderBG();
+    var randomHeader = getRandomHeaderBG();
+    $("#container > header").css("background-image", randomHeader);
+    console.debug("setRandomHeaderBG path: %s", randomHeader);
+
 });
