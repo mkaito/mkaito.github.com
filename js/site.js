481f6e9a36a0ef10c265ab15317d95f2114d673f
Chris Webstar <chris.webstar@gmail.com>
2010-07-10 16:54:14 +0100

    Added a js function to generate tables of content for articles

--- js/site.js
+++ js/site.js
@@ -4,21 +4,45 @@
 ////
 // Selects a random element from a predefined array of possible backgrounds
 // and constructs a css property with the result.
+//
 var getRandomHeaderBG = function() {
-    	var backgrounds = ['bg_steampunk_01.jpg',
-			   'bg_steampunk_02.jpg',
-			   'bg_steampunk_03.jpg'];
+    var backgrounds = ['bg_steampunk_01.jpg',
+		       'bg_steampunk_02.jpg',
+		       'bg_steampunk_03.jpg'];
 	
-	var randBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-	var randomHeaderBgPath = "url('/images/site/headers/" + randBg + "')";
-	return randomHeaderBgPath;
+    return "url('/images/site/headers/" + backgrounds.getRandom() + "')";
+};
+
+var insertTOC = function() {
+    var articleHeader = $$("article > header")[0];
+    var toc = new Element('div', {id: 'toc'});
+    var tocList = new Element('ul').inject(toc);
+    
+    var toBeTOCed = $$("article > h2, article > h3, article > h4");
+    if (toBeTOCed.length < 2) return false;
+    
+    toBeTOCed.each(function(e) {
+	var li = new Element('li');
+	var a = new Element('a', {html: e.innerHTML,
+				  href: "#" + e.id});
+	
+	a.inject(li);
+	li.inject(tocList);
+    });
+    
+    toc.inject(articleHeader, "after");
 };
 
 ////
 // main()
+//
 window.addEvent('domready', function() {
     // Set a random header image
     var randomHeader = getRandomHeaderBG();
     $("container").getElement("header").setStyle("background-image", randomHeader);
-    console.debug("Random Header Image: %s", randomHeader);
+    // console.debug("Random Header Image: %s", randomHeader);
+
+    // Generating a toc for articles
+    insertTOC();
 });
+
